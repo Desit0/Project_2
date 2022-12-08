8898eb7d1fb9d27540dbb7cf934ecf548db90e57
Desit0 <desislavapd@students.npmg.org>
2022-12-08 21:31:47 +0200

    Hello World

---
+++ Project_Desi_Mitko/src/project_desi_mitko/Project_Desi_Mitko.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project_desi_mitko;
+
+/**
+ *
+ * @author Desi
+ */
+public class Project_Desi_Mitko {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
