4e33de340d9c35669f304b83f000c473c32764ed
Mitko05 <119730812+Mitko05@users.noreply.github.com>
2022-12-09 10:50:31 +0200

    Update Project_1srok_school__.java

--- Project_1srok_school__/src/project_1srok_school__/Project_1srok_school__.java
+++ Project_1srok_school__/src/project_1srok_school__/Project_1srok_school__.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package project_1srok_school__;
-
+import java.util.scanner;
 /**
  *
  * @author Desi
@@ -14,11 +14,14 @@ public class Project_1srok_school__ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
         System.out.println("Hello java");
         System.out.println("hello.....");
         System.out.println("sss");
         System.out.print("promenqm koda");
         int a = 0;
+        int n=s.nextDouble();
+        System.out.print(n);
     }
     
 }
